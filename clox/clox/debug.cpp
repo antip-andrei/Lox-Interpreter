1131c82eb5339f3729881f05a9219ed6b6ffc71a
Andrei Antip <aligatrone@gmail.com>
2025-06-08 18:53:45 +0300

    added virtual machine

--- clox/clox/debug.cpp
+++ clox/clox/debug.cpp
@@ -36,12 +36,30 @@ int disassembleInstruction(Chunk* chunk, int offset) {
 
 	uint8_t instruction = chunk->code[offset];
 	switch (instruction) {
-		case OP_CONSTANT:
+		case OP_CONSTANT: {
 			return constantInstruction("OP_CONSTANT", chunk, offset);
-		case OP_RETURN:
+		} break;
+		case OP_NEGATE: {
+			return simpleInstruction("OP_NEGATE", offset);
+		} break;
+		case OP_ADD: {
+			return simpleInstruction("OP_ADD", offset);
+		} break;
+		case OP_SUBTRACT: {
+			return simpleInstruction("OP_SUBTRACT", offset);
+		} break;
+		case OP_MULTIPLY: {
+			return simpleInstruction("OP_MULTIPLY", offset);
+		} break;
+		case OP_DIVIDE: {
+			return simpleInstruction("OP_DIVIDE", offset);
+		} break;
+		case OP_RETURN: {
 			return simpleInstruction("OP_RETURN", offset);
-		default:
+		} break;
+		default: {
 			printf("Unknown opcode %d\n", instruction);
 			return offset + 1;
+		} break;
 	}
 }
