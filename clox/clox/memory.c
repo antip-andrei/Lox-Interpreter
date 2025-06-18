96ded123c5e2472bcb0895738969458269bfd518
Andrei Antip <aligatrone@gmail.com>
2025-06-18 16:36:14 +0300

    added closure for functions part2

--- clox/clox/memory.c
+++ clox/clox/memory.c
@@ -19,6 +19,8 @@ void* reallocate(void* pointer, size_t oldSize, size_t newSize) {
 static void freeObject(Obj* object) {
     switch (object->type) {
         case OBJ_CLOSURE: {
+            ObjClosure* closure = (ObjClosure*)object;
+            FREE_ARRAY(ObjClosure*, closure->upvalues, closure->upvalueCount);
             FREE(ObjClosure, object);
         } break;
         case OBJ_FUNCTION: {
@@ -33,6 +35,9 @@ static void freeObject(Obj* object) {
             ObjString* string = (ObjString*)object;
             FREE_ARRAY(char, string->chars, string->length + 1);
             FREE(ObjString, object);
+        } break;
+        case OBJ_UPVALUE: {
+            FREE(ObjUpvalue, object);
         } break;
     }
 }
